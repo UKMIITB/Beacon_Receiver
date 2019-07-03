fdfc74bdead1ac5e0b75f6b829c7c3a01e46b4ab
UKMIITB <ujjwalkr.maharana@gmail.com>
2019-07-03 14:37:57 +0530

    final changes

--- app/src/main/java/org/altbeacon/beaconreference/BeaconReferenceApplication.java
+++ app/src/main/java/org/altbeacon/beaconreference/BeaconReferenceApplication.java
@@ -1,17 +1,8 @@
 package org.altbeacon.beaconreference;
 
 import android.app.Application;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.app.TaskStackBuilder;
-import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
-
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
 import org.altbeacon.beacon.Region;
@@ -19,175 +10,60 @@ import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
 import org.altbeacon.beacon.startup.RegionBootstrap;
 import org.altbeacon.beacon.startup.BootstrapNotifier;
 
-/**
- * Created by dyoung on 12/13/13.
- */
 public class BeaconReferenceApplication extends Application implements BootstrapNotifier {
-    private static final String TAG = "BeaconReferenceApp";
+    private static final String TAG = "TAG";
     private RegionBootstrap regionBootstrap;
     private BackgroundPowerSaver backgroundPowerSaver;
-    private boolean haveDetectedBeaconsSinceBoot = false;
-    private MonitoringActivity monitoringActivity = null;
-    private String cumulativeLog = "";
 
     public void onCreate() {
+        Log.e("Beacon","Oncreate");
         super.onCreate();
+
         BeaconManager beaconManager = org.altbeacon.beacon.BeaconManager.getInstanceForApplication(this);
 
-        // By default the AndroidBeaconLibrary will only find AltBeacons.  If you wish to make it
-        // find a different type of beacon, you must specify the byte layout for that beacon's
-        // advertisement with a line like below.  The example shows how to find a beacon with the
-        // same byte layout as AltBeacon but with a beaconTypeCode of 0xaabb.  To find the proper
-        // layout expression for other beacon types, do a web search for "setBeaconLayout"
-        // including the quotes.
-        //
-        //beaconManager.getBeaconParsers().clear();
-        //beaconManager.getBeaconParsers().add(new BeaconParser().
-        //        setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+        beaconManager.getBeaconParsers().clear();
+        //adding layout for IBEACON
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+        // adding layout for ALTBEACON
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+        //adding layout for EDDYSTONE  TLM
+        beaconManager.getBeaconParsers().add(new BeaconParser()
+                .setBeaconLayout("x,s:0-1=feaa,m:2-2=20,d:3-3,d:4-5,d:6-7,d:8-11,d:12-15"));
+        //adding layout for EDDYSTONE UID
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout("s:0-1=feaa,m:2-2=00,p:3-3:-41,i:4-13,i:14-19"));
+        //adding layout for EDDYSTONE URL
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout("s:0-1=feaa,m:2-2=10,p:3-3:-41,i:4-20v"));
 
         beaconManager.setDebug(true);
 
-
-        // Uncomment the code below to use a foreground service to scan for beacons. This unlocks
-        // the ability to continually scan for long periods of time in the background on Andorid 8+
-        // in exchange for showing an icon at the top of the screen and a always-on notification to
-        // communicate to users that your app is using resources in the background.
-        //
-
-        /*
-        Notification.Builder builder = new Notification.Builder(this);
-        builder.setSmallIcon(R.drawable.ic_launcher);
-        builder.setContentTitle("Scanning for Beacons");
-        Intent intent = new Intent(this, MonitoringActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(
-                this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT
-        );
-        builder.setContentIntent(pendingIntent);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel("My Notification Channel ID",
-                    "My Notification Name", NotificationManager.IMPORTANCE_DEFAULT);
-            channel.setDescription("My Notification Channel Description");
-            NotificationManager notificationManager = (NotificationManager) getSystemService(
-                    Context.NOTIFICATION_SERVICE);
-            notificationManager.createNotificationChannel(channel);
-            builder.setChannelId(channel.getId());
-        }
-        beaconManager.enableForegroundServiceScanning(builder.build(), 456);
-
-        // For the above foreground scanning service to be useful, you need to disable
-        // JobScheduler-based scans (used on Android 8+) and set a fast background scan
-        // cycle that would otherwise be disallowed by the operating system.
-        //
-        beaconManager.setEnableScheduledScanJobs(false);
-        beaconManager.setBackgroundBetweenScanPeriod(0);
-        beaconManager.setBackgroundScanPeriod(1100);
-        */
-
-        Log.d(TAG, "setting up background monitoring for beacons and power saving");
+        Log.e(TAG, "setting up background monitoring for beacons and power saving");
         // wake up the app when a beacon is seen
         Region region = new Region("backgroundRegion",
                 null, null, null);
         regionBootstrap = new RegionBootstrap(this, region);
-
-        // simply constructing this class and holding a reference to it in your custom Application
-        // class will automatically cause the BeaconLibrary to save battery whenever the application
-        // is not visible.  This reduces bluetooth power usage by about 60%
         backgroundPowerSaver = new BackgroundPowerSaver(this);
 
-        // If you wish to test beacon detection in the Android Emulator, you can use code like this:
-        // BeaconManager.setBeaconSimulator(new TimedBeaconSimulator() );
-        // ((TimedBeaconSimulator) BeaconManager.getBeaconSimulator()).createTimedSimulatedBeacons();
+        Intent intent = new Intent(this, RangingActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        this.startActivity(intent);
     }
-
-    public void disableMonitoring() {
-        if (regionBootstrap != null) {
-            regionBootstrap.disable();
-            regionBootstrap = null;
-        }
-    }
-    public void enableMonitoring() {
-        Region region = new Region("backgroundRegion",
-                null, null, null);
-        regionBootstrap = new RegionBootstrap(this, region);
-    }
-
 
     @Override
     public void didEnterRegion(Region arg0) {
-        // In this example, this class sends a notification to the user whenever a Beacon
-        // matching a Region (defined above) are first seen.
-        Log.d(TAG, "did enter region.");
-        if (!haveDetectedBeaconsSinceBoot) {
-            Log.d(TAG, "auto launching MainActivity");
-
-            // The very first time since boot that we detect an beacon, we launch the
-            // MainActivity
-            Intent intent = new Intent(this, MonitoringActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            // Important:  make sure to add android:launchMode="singleInstance" in the manifest
-            // to keep multiple copies of this activity from getting created if the user has
-            // already manually launched the app.
-            this.startActivity(intent);
-            haveDetectedBeaconsSinceBoot = true;
-        } else {
-            if (monitoringActivity != null) {
-                // If the Monitoring Activity is visible, we log info about the beacons we have
-                // seen on its display
-                logToDisplay("I see a beacon again" );
-            } else {
-                // If we have already seen beacons before, but the monitoring activity is not in
-                // the foreground, we send a notification to the user on subsequent detections.
-                Log.d(TAG, "Sending notification.");
-                sendNotification();
-            }
-        }
-
-
+        Log.e(TAG, "did enter region.");
     }
 
     @Override
     public void didExitRegion(Region region) {
-        logToDisplay("I no longer see a beacon.");
+        Log.e("TAG", "I no longer see a beacon.");
     }
 
     @Override
     public void didDetermineStateForRegion(int state, Region region) {
-        logToDisplay("Current region state is: " + (state == 1 ? "INSIDE" : "OUTSIDE ("+state+")"));
+        Log.e("TAG", "Current region state is: " + (state == 1 ? "INSIDE" : "OUTSIDE ("+state+")"));
     }
-
-    private void sendNotification() {
-        NotificationCompat.Builder builder =
-                new NotificationCompat.Builder(this)
-                        .setContentTitle("Beacon Reference Application")
-                        .setContentText("An beacon is nearby.")
-                        .setSmallIcon(R.drawable.ic_launcher);
-
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-        stackBuilder.addNextIntent(new Intent(this, MonitoringActivity.class));
-        PendingIntent resultPendingIntent =
-                stackBuilder.getPendingIntent(
-                        0,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
-        builder.setContentIntent(resultPendingIntent);
-        NotificationManager notificationManager =
-                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(1, builder.build());
-    }
-
-    public void setMonitoringActivity(MonitoringActivity activity) {
-        this.monitoringActivity = activity;
-    }
-
-    private void logToDisplay(String line) {
-        cumulativeLog += (line + "\n");
-        if (this.monitoringActivity != null) {
-            this.monitoringActivity.updateLog(cumulativeLog);
-        }
-    }
-
-    public String getLog() {
-        return cumulativeLog;
-    }
-
 }
--- app/src/main/java/org/altbeacon/beaconreference/RangingActivity.java
+++ app/src/main/java/org/altbeacon/beaconreference/RangingActivity.java
@@ -1,61 +1,160 @@
 package org.altbeacon.beaconreference;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
-
+import java.util.UUID;
 import android.app.Activity;
-
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.RemoteException;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.widget.EditText;
-
-import org.altbeacon.beacon.AltBeacon;
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
-import org.altbeacon.beacon.BeaconParser;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
 
 public class RangingActivity extends Activity implements BeaconConsumer {
     protected static final String TAG = "RangingActivity";
     private BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
+    ArrayList<Data_send> datasend;
+    MediaPlayer mediaPlayer;
+    private MyListAdapter adapter;
+    private RecyclerView recyclerView;
+    public final static String MODULE_MAC = "00:18:E4:40:00:06";
+    public final static int REQUEST_ENABLE_BT = 1;
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+    BluetoothAdapter bta;                 //bluetooth stuff
+    BluetoothSocket mmSocket;             //bluetooth stuff
+    BluetoothDevice mmDevice;             //bluetooth stuff
+    private lightOn btt = null;
+    private BroadcastReceiver broadcastReceiver;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.e("Ranging", "Just inside oncreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranging);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.kabir_singh_ringtone);
+        datasend = new ArrayList<>();
+
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                final String action = intent.getAction();
+                Log.e("BroadCast reciever","in recieved method");
+                if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                    final int bluetoothState = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,
+                            BluetoothAdapter.ERROR);
+
+                    Log.e("BroadCast reciever","Adapter state changed");
+                    switch (bluetoothState) {
+                        case BluetoothAdapter.STATE_OFF: {
+                            Log.e("BroadCast reciever","Adapter state off");
+                            BluetoothConnect();
+                            break;
+                        }
+
+                    }
+                }
+                if(action.equals(BluetoothDevice.ACTION_BOND_STATE_CHANGED))
+                {
+                    Log.e("BroadCast reciever","bond state changed");
+                    final int ConnectionModuleState = intent.getIntExtra(BluetoothDevice.EXTRA_BOND_STATE,BluetoothDevice.ERROR);
+                    final String RemoteDevice = intent.getStringExtra(BluetoothDevice.EXTRA_NAME);
+                    switch (ConnectionModuleState) {
+                        case BluetoothDevice.BOND_NONE:
+                            Log.e("BroadCast reciever","device not bonded");
+                            BluetoothConnect();
+
+                    }
+                }
+            }
+        };
+        BluetoothConnect();
     }
 
-    @Override 
+    @Override
     protected void onDestroy() {
+        unregisterReceiver(broadcastReceiver);
         super.onDestroy();
     }
 
-    @Override 
+    @Override
     protected void onPause() {
         super.onPause();
         beaconManager.unbind(this);
     }
 
-    @Override 
+    @Override
     protected void onResume() {
         super.onResume();
         beaconManager.bind(this);
+        registerReceiver(broadcastReceiver,new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
+        registerReceiver(broadcastReceiver,new IntentFilter(BluetoothDevice.ACTION_BOND_STATE_CHANGED));
     }
 
     @Override
     public void onBeaconServiceConnect() {
+        Log.e("Ranging", "Inside OnBeaconServiceConnect");
 
         RangeNotifier rangeNotifier = new RangeNotifier() {
-           @Override
-           public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-              if (beacons.size() > 0) {
-                  Log.d(TAG, "didRangeBeaconsInRegion called with beacon count:  "+beacons.size());
-                  Beacon firstBeacon = beacons.iterator().next();
-                  logToDisplay("The first beacon " + firstBeacon.toString() + " is about " + firstBeacon.getDistance() + " meters away.");
-              }
-           }
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                Log.e("Ranging", "Beacon Size" + beacons.size());
+                if (beacons.size() > 0) {
+                    Log.d(TAG, "didRangeBeaconsInRegion called with beacon count:  " + beacons.size());
+                    Beacon firstBeacon = beacons.iterator().next();
+                    Data_send data_send = new Data_send();
+                    data_send.setVeh_num(firstBeacon.getId1().toString());
+                    boolean flag = true;
+                    long currenttime = System.currentTimeMillis();
+                    for(int i=0;i<datasend.size();i++)
+                    {
+                        if(datasend.get(i).getVeh_num().equalsIgnoreCase(firstBeacon.getId1().toString())
+                        && (currenttime-datasend.get(i).getTime()<=30000))
+                            flag = false;
+                    }
+
+
+                    if (data_send.getStatus().equals("") && flag)
+                    {
+                        Log.e("Ranging", "Inside data_send.getstatus==null");
+                       /* String address = firstBeacon.getBluetoothAddress();
+                        Log.e("Ranging", "Address"+ address);*/
+                        datasend.add(0, data_send);
+                        adapter.notifyDataSetChanged();
+
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+
+                            public void run() {
+                                recyclerView.scrollToPosition(0);
+                                String sendtxt = "RY";
+                                btt.write(sendtxt.getBytes());
+
+                            }
+                        }, 200);
+                        if (!mediaPlayer.isPlaying())
+                            mediaPlayer.start();
+                    }
+
+                }
+            }
 
         };
         try {
@@ -63,15 +162,65 @@ public class RangingActivity extends Activity implements BeaconConsumer {
             beaconManager.addRangeNotifier(rangeNotifier);
             beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
             beaconManager.addRangeNotifier(rangeNotifier);
-        } catch (RemoteException e) {   }
+        } catch (RemoteException e) {
+            Log.e("Ranging", "Remote exception error");
+        }
     }
 
-    private void logToDisplay(final String line) {
-        runOnUiThread(new Runnable() {
-            public void run() {
-                EditText editText = (EditText)RangingActivity.this.findViewById(R.id.rangingText);
-                editText.append(line+"\n");
+    public MediaPlayer getMediaPlayer() {
+        return mediaPlayer;
+    }
+
+    private void  BluetoothConnect()
+    {
+        Log.e("Ranging", "Inside bluetoothconnect");
+        bta = BluetoothAdapter.getDefaultAdapter();
+        if (!bta.isEnabled()) {
+            Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBTIntent, REQUEST_ENABLE_BT);
+        } else {
+            Log.e("Ranging", "calling initiate bluetooth process");
+            initiateBluetoothProcess();
+        }
+    }
+    private void initiateBluetoothProcess() {
+
+        Log.e("Ranging" ,"Inside initiatebluetoothprocess");
+        if (bta.isEnabled()) {
+            //attempt to connect to bluetooth module;
+
+            Log.i("bluetooth enabled","entered into inititate");
+            mmDevice = bta.getRemoteDevice(MODULE_MAC);
+            //Log.i("device name",mmDevice.getName());
+            //creating socket and connecting to it
+
+            try {
+                mmSocket = mmDevice.createRfcommSocketToServiceRecord(MY_UUID);
+                Log.i("mmDevice ", "got the socket");
+                mmSocket.connect();
+                Log.i("[BLUETOOTH]", "Connected to: " + mmDevice.getName());
+
+
+            } catch (IOException e) {
+                Log.e("creating socket", "error in creating socket");
+                Log.e("creating socket", e.getMessage());
+                initiateBluetoothProcess();
             }
-        });
+
+            Log.e("ranging","recycler view stuffs");
+            btt = new lightOn(mmSocket);
+            recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+            adapter = new MyListAdapter(datasend,this, mmSocket,btt);
+            recyclerView.setHasFixedSize(true);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            recyclerView.setAdapter(adapter);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_ENABLE_BT)
+            initiateBluetoothProcess();
     }
 }
+
